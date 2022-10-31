370fc57a9bc1eeaf50b7fbb972f9a351d67757b9
Sai Priya <91816433+sp0008@users.noreply.github.com>
2022-10-31 13:05:23 +0530

    Update index.js
    Minor update - functionality added to reset button

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-// const a=document.getElementByClassName("btn");
-// const b=document.getElementByClassName("images");
-
-//  i=1;
-// a.addEventListener("click", function(){
-//     i++;
-//     b.innerHTML +=`<img src="https://picsum.photos/200/300?random=${i}">`;
-// });
-
-
 const a=document.querySelector(".btn");
 const b=document.querySelector(".images");
+const c=document.querySelector(".btn1");
 
 i=1;
 
 a.addEventListener("click", function(){
     i++;
     b.innerHTML +=`<img src="https://picsum.photos/200/300?random=${i}">`;
-})
+});
+
+c.addEventListener("click", function(){
+b.innerHTML="";
+});
